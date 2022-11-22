ea48f475106161fec5c4d6e0bd2be0c9a6301517
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-11-22 19:49:34 -0300

    button size large

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,15 +8,35 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, 'identifyHand'),
-              child: const Text('Descubra sua mão')),
-          ElevatedButton(
-              onPressed: () => Navigator.pushNamed(context, 'whichHand'),
-              child: const Text('Treinar mão')),
+        children: const [
+          _button(route: 'identifyHand', label: 'Descubra sua mão'),
+          _button(route: 'whichHand', label: 'Treinar mão'),
+          _button(route: 'rankingHand', label: 'Ranking das mãos'),
         ],
       ),
+    );
+  }
+}
+
+class _button extends StatelessWidget {
+  final String route;
+  final String label;
+
+  const _button({
+    Key? key,
+    required this.route,
+    required this.label,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 16),
+      width: 180,
+      height: 60,
+      child: ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, route),
+          child: Text(label)),
     );
   }
 }
