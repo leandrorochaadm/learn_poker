d18c4f041175ffcd549286fe9315d8c181585a18
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2022-11-22 02:39:18 -0300

    card playing - adjust display

--- lib/card_playing.dart
+++ lib/card_playing.dart
@@ -21,11 +21,11 @@ class _CardPlayingState extends State<CardPlaying> {
             ? Colors.red
             : Colors.black;
     return Padding(
-      padding: EdgeInsets.only(bottom: height * 0.04, right: height * 0.01),
+      padding: EdgeInsets.only(bottom: height * 0.04, right: 10),
       child: Row(children: [
         Text(
           "${widget.card.label}${widget.card.SuitIcon()}",
-          style: TextStyle(color: _color, fontSize: 24),
+          style: TextStyle(color: _color, fontSize: 22),
         )
       ]),
     );
